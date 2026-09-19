f86c6f72c1924063d800a8cae1abc82fa654184a
Labros Chaidas <lampros@chaidas.com>
2026-09-19 13:51:44 -0400

    Bump version to 0.9.1190

--- src/krikri/version.cr
+++ src/krikri/version.cr
@@ -1,7 +1,7 @@
 require "yaml"
 
 module Krikri
-  VERSION = "0.9.1189"
+  VERSION = "0.9.1190"
 
   # Baked into the binary at compile time (never read from disk at
   # runtime - a deployed binary has no shard.lock beside it), the same
