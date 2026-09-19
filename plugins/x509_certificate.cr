a7bf96c22a27f8128c5743d013b254aa4f3f422c
Labros Chaidas <lampros@chaidas.com>
2026-09-18 20:10:43 -0400

    Remove not_nil! from plugins/x509_certificate.cr (ameba Lint/NotNil)

--- plugins/x509_certificate.cr
+++ plugins/x509_certificate.cr
@@ -98,8 +98,7 @@ module Krikri
 
       temp_files = [] of String
       begin
-        path = @params["path"]?
-        path = expand_tilde(path.not_nil!)
+        path = expand_tilde(@params["path"])
         state = @params["state"]? || "present"
         check_mode = true?(@params["_ansible_check_mode"]?)
 
