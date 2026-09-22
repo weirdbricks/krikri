7f759cbd9fba104633fa7bebec55fe2b69da7a4a
Labros Chaidas <lampros@chaidas.com>
2026-09-22 19:02:54 -0400

    Bump version to 0.9.1259 (ansible_diff_mode magic var fix)

--- src/krikri/version.cr
+++ src/krikri/version.cr
@@ -1,7 +1,7 @@
 require "yaml"
 
 module Krikri
-  VERSION = "0.9.1258"
+  VERSION = "0.9.1259"
 
   # Baked into the binary at compile time (never read from disk at
   # runtime - a deployed binary has no shard.lock beside it), the same
