f993172a844eabeb75c8a3f8b49adae345d35a23
Labros Chaidas <lampros@chaidas.com>
2026-09-23 21:55:17 -0400

    Bump version to 0.9.1275

--- src/krikri/version.cr
+++ src/krikri/version.cr
@@ -1,7 +1,7 @@
 require "yaml"
 
 module Krikri
-  VERSION = "0.9.1274"
+  VERSION = "0.9.1275"
 
   # Baked in at compile time via the same `--release` flag `build.sh`
   # passes through to `crystal build`. A timing-sensitive round run
